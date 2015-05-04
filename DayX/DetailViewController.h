d48bf026a97f5967378f4e54050419f3143c877c
sarah <lingwall76@gmail.com>
2015-05-04 15:52:29 -0600

    Commit2

---
+++ DayX/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  DayX
+//
+//  Created by Sarah on 5/4/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
